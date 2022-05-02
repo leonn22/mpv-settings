c94ec5feff97cb5c61d6bf86b1114ece677213e1
Adrian Price <adroitp@pm.me>
2022-05-03 00:38:25 +0530

    clean up

--- scripts/boss-key.lua
+++ scripts/boss-key.lua
@@ -15,37 +15,8 @@
 
 utils = require 'mp.utils'
 
-local platform = nil --set to 'linux', 'windows' or 'macos' to override automatic assign
-if not platform then
-    local o = {}
-    if mp.get_property_native('options/vo-mmcss-profile', o) ~= o then
-        platform = 'windows'
-    elseif mp.get_property_native('options/input-app-events', o) ~= o then
-        platform = 'macos'
-    else
-        platform = 'linux'
-    end
-end
+local platform = windows --set to 'linux', 'windows' or 'macos' to override automatic assign
 
--- TODO: macOS implementation?
-function boss_key()
-    mp.set_property_native("pause", true)
-    if platform == 'windows' then
-        mp.command([[run cmd /c echo m > \\.\pipe\mpv-boss-key-]]..utils.getpid())
-    elseif platform == 'linux' then
-        utils.subprocess({ args = {'xdotool', 'getactivewindow', 'windowminimize'} })
-    end
-end
-
--- The only way to minimize the window in Windows is through a compiled Win32
--- API call. So we open an async powershell session, define the function call,
--- compile it, and then wait for a signal from this script to execute the call.
--- Signaling is done through named pipes, which to my surprise were present on
--- Windows. Not to my surprise, they didn't work reliably. Writing to the pipe
--- from PS or CMD yields different results, for example. In addition, PS's
--- Events and other async were extremely finnicky. Because of these reasons,
--- and after many, many rewrites, I've arrived at the unorthodox mess that is
--- the code below. It's not pretty, but at l(e)ast it works reliably.
 if platform == 'windows' then
     mp.command_native_async({
         name = "subprocess",
